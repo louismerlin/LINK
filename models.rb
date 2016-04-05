ea058b89fb1c6f28693dd5fcbeb7e4ecd9fee537
louismerlin <louis.merlin@epfl.ch>
2016-04-05 12:08:38 +0200

    Make small changes

--- models.rb
+++ models.rb
@@ -1,11 +1,20 @@
-if !File.exists?("./boilerplate.db") || settings.environment == :test
+if !File.exists?("./link.db") || settings.environment == :test
 
-  # IF YOU DO CHANGES HERE, DELETE THE FILE 'boilerplate.db'
+  # IF YOU DO CHANGES HERE, DELETE THE FILE 'link.db'
 
   if settings.environment == :test
     DB = Sequel.sqlite
   else
-    DB = Sequel.connect("sqlite://boilerplate.db")
+    DB = Sequel.connect("sqlite://link.db")
+  end
+
+  DB.create_table :users do
+    primary_key :id
+    String      :first_name
+    String      :last_name
+    String      :email
+    String      :username
+    String      :password_digest
   end
 
   DB.create_table :channels do
@@ -14,31 +23,30 @@ if !File.exists?("./boilerplate.db") || settings.environment == :test
     foreign_key :user_id
   end
 
-  DB.create_table :users do
-    primary_key :id
-    String      :first_name
-    String      :last_name
-    String      :email
-    String      :password_digest
-    foreign_key :user_id
-
-  end
-
   DB.create_table :links do
     primary_key :id
-    String      :#HOW TO STORE AN URL?#
-    Datetime    :h
+    String      :url
+    String      :description
+    Datetime    :sent
+    Datetime    :read
   end
 
   DB.create_table :groups do
 
   end
 
+  DB.create_join_table(:user_id=>)
+
 
 else
-  DB = Sequel.connect("sqlite://boilerplate.db")
+  DB = Sequel.connect("sqlite://link.db")
 end
 
+class User < Sequel::Model
+  plugin :secure_password
+  many_to_many :Channels
+  #many_to_many : users?#
+end
 
 class Channel < Sequel::Model
   many_to_many :users
@@ -51,12 +59,4 @@ end
 
 class Conversation < Channel
   many_to_many :users
-end
-
-
-
-class User < Sequel::Model
-  plugin :secure_password
-  many_to_many :Channels
-  #many_to_many : users?#
 end
