a15d601f7b60969383ef2cc5505f2b9e2f3169de
Hugo Roussel <hugo@localhost.localdomain>
2016-04-05 11:50:22 +0200

    Models.rb update. Lot of things to do, includign verifying the way inheritance implemented for channels works

--- models.rb
+++ models.rb
@@ -8,26 +8,55 @@ if !File.exists?("./boilerplate.db") || settings.environment == :test
     DB = Sequel.connect("sqlite://boilerplate.db")
   end
 
-  DB.create_table :breads do
+  DB.create_table :channels do
     primary_key :id
-    String      :type
+    String      :name
+    foreign_key :user_id
   end
 
-  DB.create_table :slices do
+  DB.create_table :users do
     primary_key :id
-    Float       :width
-    foreign_key :bread_id
+    String      :first_name
+    String      :last_name
+    String      :email
+    String      :password_digest
+    foreign_key :user_id
+
   end
+
+  DB.create_table :links do
+    primary_key :id
+    String      :#HOW TO STORE AN URL?#
+    Datetime    :h
+  end
+
+  DB.create_table :groups do
+
+  end
+
 
 else
   DB = Sequel.connect("sqlite://boilerplate.db")
 end
 
 
-class Bread < Sequel::Model
-  one_to_many :slices
+class Channel < Sequel::Model
+  many_to_many :users
+end
+#FIND A WAY TO CREATE #
+
+class Group < Channel
+  many_to_many :users
 end
 
-class Slice < Sequel::Model
-  many_to_one :bread
+class Conversation < Channel
+  many_to_many :users
+end
+
+
+
+class User < Sequel::Model
+  plugin :secure_password
+  many_to_many :Channels
+  #many_to_many : users?#
 end
